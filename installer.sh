7a74c4cfd767c6caeed4c83f2bb1484a410b6b87
simonmoulds <sim.moulds@gmail.com>
2020-08-04 08:24:00 +0100

    updating installer

--- installer.sh
+++ installer.sh
@@ -2,19 +2,15 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# clean
-rm -rf ~/.emacs.d
-rm -rf ~/.zprezto
-rm -rf ~/.tmux
-rm -r ~/.zpreztorc
-rm -r ~/.zshrc ~/.zlogin ~/.zlogout ~/.zprofile ~/.zshenv
-rm -r ~/.tmux.conf
-rm -r ~/.gitconfig
-
+# ###################### #
 # emacs
-ln -s ${BASEDIR}/emacs_d/ ~/.emacs.d
+# ###################### #
 
+ln -s -f "${BASEDIR}"/emacs_d/ "${HOME}"/.emacs.d
+
+# ###################### #
 # zsh
+# ###################### #
 
 # N.B. the zprezto directory is a fork of the original repo
 # (https://github.com/sorin-ionescu/prezto.git). To download the
@@ -29,33 +25,47 @@ ln -s ${BASEDIR}/emacs_d/ ~/.emacs.d
 # git fetch upstream
 # cd "${ZDOTDIR:-$HOME}"/dotfiles
 
-# NB to remove submodules, see the following gist: https://gist.github.com/simonmoulds/3d6d07f0703bfe5a9ccc6744668b9187
+# NB to remove submodules, see the following gist:
+# https://gist.github.com/simonmoulds/3d6d07f0703bfe5a9ccc6744668b9187
 
 # this is a bit convoluted but it preserves the original
 # file structure of the prezto repository
-for f in zshrc zlogin zlogout zprofile zshenv zpreztorc
+for FILE in zshrc zlogin zlogout zprofile zshenv zpreztorc
 do    
-    cp -rf ${BASEDIR}/${f} ${BASEDIR}/prezto/runcoms
+    cp -f "${BASEDIR}"/"${FILE}" "${BASEDIR}"/prezto/runcoms
 done
 
-cp -rf ${BASEDIR}/prezto ~/.zprezto
+if [ -d "${HOME}"/.zprezto ]
+then    
+    rm -rf "${HOME}"/.zprezto
+fi
+cp -rf "${BASEDIR}"/prezto "${HOME}"/.zprezto
 
-ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
-ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
-ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
-ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
-ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
-ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -s -f "${HOME}"/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s -f "${HOME}"/.zprezto/runcoms/zshrc ~/.zshrc
+ln -s -f "${HOME}"/.zprezto/runcoms/zlogin ~/.zlogin
+ln -s -f "${HOME}"/.zprezto/runcoms/zlogout ~/.zlogout
+ln -s -f "${HOME}"/.zprezto/runcoms/zprofile ~/.zprofile
+ln -s -f "${HOME}"/.zprezto/runcoms/zshenv ~/.zshenv
 
+# ###################### #
 # tmux
+# ###################### #
 
-# git submodule add https://github.com/tmux-plugins/tpm "${ZDOTDIR:-$HOME}"/dotfiles/tmux/plugins
-# cd "${ZDOTDIR:-$HOME}"/dotfiles/tmux/plugins
+# git submodule add https://github.com/simonmoulds/.tmux.git tmux
+# cd tmux
 # git submodule update --init --recursive
-# cd "${ZDOTDIR:-$HOME}"/dotfiles
+# cd ..
+if [ -d "${HOME}"/.tmux ]
+then    
+    rm -rf "${HOME}"/.tmux
+fi
+cp -rf tmux "${HOME}"/.tmux
+ln -s -f "${HOME}"/.tmux/.tmux.conf "${HOME}"/.tmux.conf
+ln -s -f "${HOME}"/.tmux/.tmux.conf.local "${HOME}"/.tmux.conf.local
 
-ln -s ${BASEDIR}/tmux/ ~/.tmux
-ln -s ${BASEDIR}/tmux_conf ~/.tmux.conf
-
+# ###################### #
 # git
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+# ###################### #
+
+ln -s -f "${BASEDIR}"/gitconfig "${HOME}"/.gitconfig
